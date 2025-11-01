124b450f4613809e7c28e4ea927fab4e035243c1
Naven Laya <navenchristian.laya@gmail.com>
2025-10-31 20:10:09 -0700

    fixed linting errors

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
 
 @app.route("/")
 def home():
@@ -11,5 +10,6 @@ def home():
         "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
     )
 
-if __name__=="__main__":
-    app.run (host="0.0.0.0", port=8080) 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
